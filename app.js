77e006b62d121bed9acf1c88a2f85b4bd51a2db4
sachin <sachinkannojiya990@gmail.com>
2023-06-25 18:04:24 +0530

    added get call

--- app.js
+++ app.js
@@ -16,8 +16,13 @@ function saveData() {
     //localStorage.setItem('appointment', appointmentJSON);
     axios.post('https://crudcrud.com/api/4eac9ffbdeb74c60beba59e835f1b727/appoinmentData', appointment)
     .then((data)=> console.log(data))
-    .then(h1.innerText=data).data
+    
     .catch((e)=> console.log(e));
+
+    axios.get('https://crudcrud.com/api/4eac9ffbdeb74c60beba59e835f1b727/appoinmentData')
+    .then((data)=> console.log(data))
+    .catch((e)=> console.log(e));
+    
   
     alert('Appointment booked successfully!');
   }
