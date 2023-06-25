6dca41d2ffefc1e76c86c25646f5994572c0e32d
sachin <sachinkannojiya990@gmail.com>
2023-06-25 17:44:49 +0530

    first messege

---
+++ app.js
@@ -0,0 +1,24 @@
+function saveData() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    const h1= document.querySelector('h1')
+  
+    // Create an object to store the data
+    var appointment = {
+      name: name,
+      email: email
+    };
+  
+    // Convert the data to a JSON string
+    var appointmentJSON = JSON.stringify(appointment);
+  
+    // Save the data to local storage
+    //localStorage.setItem('appointment', appointmentJSON);
+    axios.post('https://crudcrud.com/api/4eac9ffbdeb74c60beba59e835f1b727/appoinmentData', appointment)
+    .then((data)=> console.log(data))
+    .then(h1.innerText=data).data
+    .catch((e)=> console.log(e));
+  
+    alert('Appointment booked successfully!');
+  }
+  
